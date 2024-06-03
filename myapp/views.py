5797fa99b420b220deee681886d06798d05ba084
LLonghi02 <lara.longhi3@gmail.com>
2024-06-03 20:32:36 +0200

    definizione modelli e migrazione

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.http import JsonResponse # type: ignore
+from .models import Cittadino, Ospedale, Ricovero, Patologia, PatologiaRicovero, PatologiaCronica, PatologiaMortale
 
-# Create your views here.
+def cittadini_list(request):
+    cittadini = list(Cittadino.objects.values())
+    return JsonResponse(cittadini, safe=False)
+
+def ospedali_list(request):
+    ospedali = list(Ospedale.objects.values())
+    return JsonResponse(ospedali, safe=False)
+
+def ricoveri_list(request):
+    ricoveri = list(Ricovero.objects.values())
+    return JsonResponse(ricoveri, safe=False)
+
+def patologie_list(request):
+    patologie = list(Patologia.objects.values())
+    return JsonResponse(patologie, safe=False)
+
+def patologie_ricovero_list(request):
+    patologie_ricovero = list(PatologiaRicovero.objects.values())
+    return JsonResponse(patologie_ricovero, safe=False)
+
+def patologie_croniche_list(request):
+    patologie_croniche = list(PatologiaCronica.objects.values())
+    return JsonResponse(patologie_croniche, safe=False)
+
+def patologie_mortali_list(request):
+    patologie_mortali = list(PatologiaMortale.objects.values())
+    return JsonResponse(patologie_mortali, safe=False)
--- myproject/urls.py
+++ myproject/urls.py
@@ -14,9 +14,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.contrib import admin # type: ignore
+from django.urls import path # type: ignore
+from myapp.views import ( # type: ignore
+    cittadini_list,
+    ospedali_list,
+    ricoveri_list,
+    patologie_list,
+    patologie_ricovero_list,
+    patologie_croniche_list,
+    patologie_mortali_list,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cittadini/', cittadini_list),
+    path('ospedali/', ospedali_list),
+    path('ricoveri/', ricoveri_list),
+    path('patologie/', patologie_list),
+    path('patologie_ricovero/', patologie_ricovero_list),
+    path('patologie_croniche/', patologie_croniche_list),
+    path('patologie_mortali/', patologie_mortali_list),
 ]
