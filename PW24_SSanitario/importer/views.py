806121a16c066e7dce80f3e562bb106b1b41854c
carraraFederico <federicocarrara0602@gmail.com>
2024-07-26 14:21:57 +0200

    Rimozione stampe di controllo da Django

--- PW24_SSanitario/importer/views.py
+++ PW24_SSanitario/importer/views.py
@@ -22,34 +22,14 @@ def fetch_and_save(request):
 
     if request.method == 'POST':
         logger.info("È una richiesta POST")
-        # URL del web service remoto per ottenere i dati
-        #remote_url = 'https://servsanitariopw9.altervista.org/WS.php'
-
-        # Variabile per tenere traccia dei dati salvati
-        dati_migrati = {
-            "Cittadino": [],
-            "Ospedale": [],
-            "Patologia": [],
-            "PatologiaCronica": [],
-            "PatologiaMortale": [],
-            "Ricovero": [],
-            "PatologiaRicovero": []
-        }
 
         try:
-            #Effettua una richiesta POST per ottenere i dati
-            #response = requests.get(remote_url)
-            #response.raise_for_status()  # Solleva un'eccezione se la richiesta non ha successo
             response = requests.get('http://localhost:8000/run-migrations/')
-            #print(response.text)
 
             data = json.loads(request.body)
 
-            print("Dati migrati iniziali:", dati_migrati)
-
             # Inserisci i dati per il modello Cittadino
             for cittadino_data in data.get('Cittadino', []):  # Assicurati che il nome del campo sia corretto
-                ##print("Elaborando cittadino:", cittadino_data)  # Debugging: stampa il cittadino elaborato
                 try:
                     cittadino, created = Cittadino.objects.update_or_create(
                         CSSN=cittadino_data.get('CSSN'),
@@ -61,12 +41,9 @@ def fetch_and_save(request):
                             'indirizzo': cittadino_data.get('indirizzo')
                         }
                     )
-                    dati_migrati["Cittadino"].append(cittadino_data)
                 except Exception as e:
                     print(f"Errore durante l'elaborazione di {cittadino_data}: {e}")
                     return JsonResponse({'error': f"Errore durante l'elaborazione di {cittadino_data}: {e}"}, status=500)
-            
-            print("Dati migrati cittadini:", dati_migrati)
 
             # Inserisci i dati per il modello Ospedale
             for ospedale_data in data.get('Ospedale', []):
@@ -80,7 +57,6 @@ def fetch_and_save(request):
                             'direttoreSanitario': Cittadino.objects.get(CSSN=ospedale_data.get('direttoreSanitario'))
                         }
                     )
-                    dati_migrati["Ospedale"].append(ospedale_data)
                 except Exception as e:
                     print(f"Errore durante l'elaborazione di {ospedale_data}: {e}")
                     return JsonResponse({'error': f"Errore durante l'elaborazione di {ospedale_data}: {e}"}, status=500)
@@ -95,7 +71,6 @@ def fetch_and_save(request):
                             'criticità': patologia_data.get('criticità')
                         }
                     )
-                    dati_migrati["Patologia"].append(patologia_data)
                 except Exception as e:
                     print(f"Errore durante l'elaborazione di {patologia_data}: {e}")
                     return JsonResponse({'error': f"Errore durante l'elaborazione di {patologia_data}: {e}"}, status=500)
@@ -106,7 +81,6 @@ def fetch_and_save(request):
                     patologia_cronica, created = PatologiaCronica.objects.update_or_create(
                         codPatologia=Patologia.objects.get(cod=patologia_cronica_data.get('codPatologia')),
                     )
-                    dati_migrati["PatologiaCronica"].append(patologia_cronica_data)
                 except Exception as e:
                     print(f"Errore durante l'elaborazione di {patologia_cronica_data}: {e}")
                     return JsonResponse({'error': f"Errore durante l'elaborazione di {patologia_cronica_data}: {e}"}, status=500)
@@ -117,7 +91,6 @@ def fetch_and_save(request):
                     patologia_mortale, created = PatologiaMortale.objects.update_or_create(
                         codPatologia=Patologia.objects.get(cod=patologia_mortale_data.get('codPatologia')),
                     )
-                    dati_migrati["PatologiaMortale"].append(patologia_mortale_data)
                 except Exception as e:
                     print(f"Errore durante l'elaborazione di {patologia_mortale_data}: {e}")
                     return JsonResponse({'error': f"Errore durante l'elaborazione di {patologia_mortale_data}: {e}"}, status=500)
@@ -136,7 +109,6 @@ def fetch_and_save(request):
                             'costo': ricovero_data.get('costo')
                         }
                     )
-                    dati_migrati["Ricovero"].append(ricovero_data)
                 except Exception as e:
                     print(f"Errore durante l'elaborazione di {ricovero_data}: {e}")
                     return JsonResponse({'error': f"Errore durante l'elaborazione di {ricovero_data}: {e}"}, status=500)
@@ -149,18 +121,15 @@ def fetch_and_save(request):
                         codRicovero=Ricovero.objects.get(cod=patologia_ricovero_data.get('codRicovero')),
                         codPatologia=Patologia.objects.get(cod=patologia_ricovero_data.get('codPatologia'))
                     )
-                    dati_migrati["PatologiaRicovero"].append(patologia_ricovero_data)
                 except Exception as e:
                     print(f"Errore durante l'elaborazione di {patologia_ricovero_data}: {e}")
                     return JsonResponse({'error': f"Errore durante l'elaborazione di {patologia_ricovero_data}: {e}"}, status=500)
 
-            print("Dati migrati:", dati_migrati)
-
-            return JsonResponse({"status": "dati migrati con successo", "dati": dati_migrati})
+            return JsonResponse({"status": "dati migrati con successo"})
         except requests.RequestException as e:
             return JsonResponse({"status": "errore nel recupero dei dati", "error": str(e)}, status=500)
         except Exception as e:
-            return JsonResponse({"status": "errore durante l'inserimento dei dati", "dati": dati_migrati, "error": str(e)}, status=500)
+            return JsonResponse({"status": "errore durante l'inserimento dei dati", "error": str(e)}, status=500)
     else:
         logger.info("Non è una richiesta POST")
         return JsonResponse({"status": "Metodo non supportato"}, status=405)
